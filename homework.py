61f493c3489095b55db586cd716dbec26b36e3eb
Evgeniy Popov <Argentina43@yandex.ru>
2021-12-05 01:19:43 +0300

    на ревью 1

--- homework.py
+++ homework.py
@@ -4,6 +4,7 @@ import time
 from http import HTTPStatus
 
 import requests
+from requests.models import Response
 import telegram
 from dotenv import load_dotenv
 
@@ -17,7 +18,7 @@ PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 10  # 600
+RETRY_TIME = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 
@@ -28,6 +29,13 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
+last_response = ''
+
+
+class API_request_error(Exception):
+    """ошибка запроса АРI"""
+    pass
+
 
 def send_message(bot, message):
     """
@@ -36,8 +44,14 @@ def send_message(bot, message):
         экземпляр класса Bot и строку с текстом сообщения.
     """
     try:
-        bot.send_message(TELEGRAM_CHAT_ID, message)
-        logging.info(f'сообщение отправлено')
+        global last_response
+        if message != last_response:
+            bot.send_message(TELEGRAM_CHAT_ID, message)
+            last_response = message
+            logging.info(f'сообщение отправлено')
+        else:
+            logging.info('сообщение повторяется')
+        
     except Exception:
         logging.error(f'сбой отправки сообщения {Exception}')
 
@@ -50,13 +64,15 @@ def get_api_answer(current_timestamp):
         JSON к типам данных Python.
     """
     timestamp = current_timestamp or int(time.time())
-    # params = {'from_date': timestamp}
-    params = {'from_date': 0}
+    params = {'from_date': timestamp}
     headers={'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
-    response = requests.get(ENDPOINT,params=params, headers=headers)
-    if response.status_code != HTTPStatus.OK:
-        logging.error(f'эндпоинт недоступен: {ENDPOINT}')
-        raise Exception(f'ошибка {response}')
+    try:
+        response = requests.get(ENDPOINT,params=params, headers=headers)
+        if response.status_code != HTTPStatus.OK:
+            logging.error(f'эндпоинт недоступен: {ENDPOINT}')
+            raise Exception(f'ошибка {response}')
+    except Exception:
+        raise API_request_error('API не отвечает')
     return response.json()
 
 
@@ -68,8 +84,8 @@ def check_response(response):
         домашних работ (он может быть и пустым), доступный в ответе API
         по ключу 'homeworks'.
     """
-    if not response['homeworks'][0]:
-        raise IndexError('пустой')
+    if len(response['homeworks']) == 0:
+        raise IndexError('homeworks пуст')
     if type(response) != dict:
         raise TypeError('ожидается словарь')
     if type(response['homeworks']) != list:
@@ -119,9 +135,9 @@ def main():
     current_timestamp = int(time.time())
     while True:
         try:
-            current_timestamp = get_api_answer(current_timestamp)
-            if check_response(current_timestamp):
-                message = parse_status(current_timestamp['homeworks'][0])
+            response = get_api_answer(current_timestamp)
+            if check_response(response):
+                message = parse_status(response['homeworks'][0])
                 send_message(bot, message)
             time.sleep(RETRY_TIME)
 
